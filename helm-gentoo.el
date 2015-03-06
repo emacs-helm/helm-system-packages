8d0dd9e07b4ca97e23333426fa0a6af437dbe619
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2015-03-06 12:18:24 +0100

    * helm-gentoo.el: Add version.

--- helm-gentoo.el
+++ helm-gentoo.el
@@ -2,6 +2,7 @@
 
 ;; Copyright (C) 2012 ~ 2014 Thierry Volpiatto <thierry.volpiatto@gmail.com>
 
+;; Version: 1.6.8
 ;; Package-Requires: ((helm "1.5"))
 
 ;; This program is free software; you can redistribute it and/or modify
