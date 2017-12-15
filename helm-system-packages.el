9006ffe315854a195ab0c41ef7d65fbc75bdc644
Pierre Neidhardt <ambrevar@gmail.com>
2017-12-15 10:17:06 +0100

    readme: Add entry point and reference to jabranham's system-packages

--- helm-system-packages.el
+++ helm-system-packages.el
@@ -13,12 +13,12 @@
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -26,8 +26,6 @@
 ;; Helm UI wrapper for system package managers.
 
 ;;; Code:
-
-;;; Internals
 
 (defvar helm-system-packages-eshell-buffer "*helm-system-packages-eshell*")
 (defvar helm-system-packages-buffer "*helm-system-packages-output*")
